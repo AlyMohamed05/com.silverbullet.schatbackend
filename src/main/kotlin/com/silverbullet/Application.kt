a95566e5332a16850bbb21de6a5f446b376dd5a5
Aly <asecure05@gmail.com>
2023-02-19 17:51:47 +0200

    Initial commit

---
+++ src/main/kotlin/com/silverbullet/Application.kt
@@ -0,0 +1,17 @@
+package com.silverbullet
+
+import io.ktor.server.application.*
+import com.silverbullet.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSockets()
+    configureSerialization()
+    configureHTTP()
+    configureSecurity()
+    configureRouting()
+    configureStatusPages()
+}
---
+++ src/main/kotlin/com/silverbullet/plugins/Routing.kt
@@ -0,0 +1,8 @@
+package com.silverbullet.plugins
+
+import io.ktor.server.application.*
+
+fun Application.configureRouting() {
+
+
+}
